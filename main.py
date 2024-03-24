2361bcb245ca269abcdff8d638e6d1baad8df111
Thomas <tomplaysz123@gmail.com>
2024-03-24 12:54:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,45 +10,41 @@ def shuffle(string):
 
 # Random Uppercase Letter
 def generateUppercase():
-    global uppercaseLetter
-    uppercaseLetter = chr(random.randint(65,90))
+    uppercaseLetter = chr(random.randint(65, 90))
     return uppercaseLetter
 
 # Random Lowercase Letter
 def generateLowercase():
-    global lowercaseLetter
-    lowercaseLetter = chr(random.randint(97,122)) 
+    lowercaseLetter = chr(random.randint(97, 122)) 
     return lowercaseLetter
 
 # Random Digit 
 def generateDigit():
-    global digit
-    digit = str(random.randint(0,9))
+    digit = str(random.randint(0, 9))
     return digit
 
 # Random Punctuation Sign
 def generatePunctuation():
-    global punctuationSign
-    punctuationSign = chr(random.randint(33,47)) 
+    punctuationSign = chr(random.randint(33, 47)) 
     return punctuationSign
 
 # While Loop Conditions
-validLength = False
+totalLength = False
 uppercaseNumLength = False
 lowercaseNumLength = False
 digitNumLength = False
 
 # User Inputs Password Length
-while validLength == False:
+while totalLength == False:
     passwordLength = int(input("How long do you want your password to be? (7-50): "))
     remainingChar = passwordLength
 
     # Password Length Validation
     if passwordLength < 7 or passwordLength > 50:
         print("Invalid Length.")
-        validLength = False
+        totalLength = False
     else:
-        validLength = True
+        totalLength = True
     
 # Input Number of Uppercase Letters and Validate
 while uppercaseNumLength == False:
@@ -91,13 +87,13 @@ remainingChar -= punctuationNum
 # Create Unsorted Password
 password = ""
 
-for i in range(0,uppercaseNum):
+for i in range(0, uppercaseNum):
     password += generateUppercase()
-for i in range(0,lowercaseNum):
+for i in range(0, lowercaseNum):
     password += generateLowercase()
-for i in range(0,digitNum):
+for i in range(0, digitNum):
     password += generateDigit()
-for i in range(0,punctuationNum):
+for i in range(0, punctuationNum):
     password += generatePunctuation()
 
 # Sort and Ouput
